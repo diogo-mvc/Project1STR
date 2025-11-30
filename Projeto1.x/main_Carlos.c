182e79b950b999030153954273ecff98a04d6de6
Barbichas <oeinsteineracareca@gmail.com>
2025-11-30 18:21:04 +0000

    mais cenas no main alternativo

--- Projeto1.x/main_Carlos.c
+++ Projeto1.x/main_Carlos.c
@@ -52,7 +52,7 @@
 
 //extern volatile bool second_has_passed;
 
-/*--------------------------------  MACROS -------------------------------------*/
+/*--------------------------------  MACROS(values for variable init mostly) -------------------------------------*/
 #define PMON 5   /*5 sec monitoring period*/
 #define TALA 3   /*3 seconds duration of alarm signal (PWM)*/
 #define TINA 10  /*10 sec inactivity time*/
@@ -69,8 +69,38 @@
 #define CONFIG_MODE 1
 #define RECORD_MODE 2
 
+#define NO_ALARM 0
+#define TIME_ALARM 1
+#define TEMP_ALARM 2
+#define LIGHT_ALARM 3
+
+
 /*------------------GLOBAL VARIABLES(good for interrupt handling) ----------------*/
-extern volatile int curr_mode = NORMAL_MODE;
+extern volatile int curr_mode        = NORMAL_MODE;
+extern volatile int alarm_flag       = NO_ALARM;
+extern volatile int inactive_time      = 0;  /*for counting the time in record mode*/
+extern volatile int pwm_active_time    = -1; /*start at -1 to even out the increments*/
+
+
+extern volatile bool S1_pressed      = false;
+extern volatile bool S2_pressed      = false;
+extern volatile bool alarms_enabled  = false;
+
+extern volatile int alarm_hour   = ALAS;
+extern volatile int alarm_minute = ALAM;
+extern volatile int alarm_second = ALAH;
+extern volatile int temp_alarm   = ALAT;
+extern volatile int light_alarm  = ALAL;
+
+
+
+extern volatile struct SensorData{
+  unsigned long seconds = 0;
+  unsigned long hours, minutes, secs;
+  unsigned char temp;
+  unsigned char light;
+}
+
 
 /*---- Function primitves(in C we must declare stuff before calling them, definition is a the bottom)*/
 void resolve_NORMAL_MODE(void);
@@ -79,46 +109,48 @@ void resolve_RECORD_MODE(void);
 void resolve_OUT_OF_BOUNDS(void);
 
 
+
 unsigned char readTC74 (void)
 {
 	unsigned char value;
-do{
-	IdleI2C();
-	StartI2C(); IdleI2C();
-    
-	WriteI2C(0x9a | 0x00); IdleI2C();
-	WriteI2C(0x01); IdleI2C();
-	RestartI2C(); IdleI2C();
-	WriteI2C(0x9a | 0x01); IdleI2C();
-	value = ReadI2C(); IdleI2C();
-	NotAckI2C(); IdleI2C();
-	StopI2C();
-} while (!(value & 0x40));
+  do{
+    IdleI2C();
+    StartI2C(); IdleI2C();
+      
+    WriteI2C(0x9a | 0x00); IdleI2C();
+    WriteI2C(0x01); IdleI2C();
+    RestartI2C(); IdleI2C();
+    WriteI2C(0x9a | 0x01); IdleI2C();
+    value = ReadI2C(); IdleI2C();
+    NotAckI2C(); IdleI2C();
+    StopI2C();
+  } while (!(value & 0x40));
 
-	IdleI2C();
-	StartI2C(); IdleI2C();
-	WriteI2C(0x9a | 0x00); IdleI2C();
-	WriteI2C(0x00); IdleI2C();
-	RestartI2C(); IdleI2C();
-	WriteI2C(0x9a | 0x01); IdleI2C();
-	value = ReadI2C(); IdleI2C();
-	NotAckI2C(); IdleI2C();
-	StopI2C();
+    IdleI2C();
+    StartI2C(); IdleI2C();
+    WriteI2C(0x9a | 0x00); IdleI2C();
+    WriteI2C(0x00); IdleI2C();
+    RestartI2C(); IdleI2C();
+    WriteI2C(0x9a | 0x01); IdleI2C();
+    value = ReadI2C(); IdleI2C();
+    NotAckI2C(); IdleI2C();
+    StopI2C();
 
 	return value;
 }
 
+unsigned char readLight(void){
+  
+  return readTC74();
+
+}
+
 void main(void)
 {    
-    unsigned char c;
-    char buf[17];
-
     // initialize the device
     SYSTEM_Initialize();
 
 
-    unsigned long seconds = 0;
-    unsigned long hours, minutes, secs;
     // When using interrupts, you need to set the Global and Peripheral Interrupt Enable bits
     // Use the following macros to:
 
@@ -149,43 +181,90 @@ void main(void)
       else if(curr_mode == RECORD_MODE){resolve_RECORD_MODE();}
       else{resolve_OUT_OF_BOUNDS();}
         
-        if (second_has_passed==true) {
-          second_has_passed = false;
-          seconds++;
-          hours = (seconds / 3600);
-          minutes = (seconds / 60) % 60;
-          secs = seconds % 60;
-
-          LCDcmd(0x80);       //first line, first column
-          sprintf(buf, "Time %02lu:%02lu:%02lu", hours, minutes, secs);
-          LCDstr(buf);
-          while (LCDbusy());
-        }//
-        
-        IO_RA7_Toggle();
-        __delay_ms(1000);
-        // SLEEP();
-        //NOP();
-        
-        
-
-
-        /*c = readTC74();
-        LCDcmd(0x80);       //first line, first column
-        while (LCDbusy());
-        LCDstr("Temp");
-        LCDpos(0,8);
-        while (LCDbusy());
-        LCDstr("STR-RTS");
-        LCDcmd(0xc0);       // second line, first column
-        sprintf(buf, "%02d C", c);
-        while (LCDbusy());
-        LCDstr(buf);
-        __delay_ms(2000); */
     }
 }
 
-void resolve_NORMAL_MODE(){}
+void resolve_NORMAL_MODE(){
+
+  char buf[17]; /*buffer to write to LCD display(maybe make it not local?)*/
+
+  if (second_has_passed==true) {
+    
+    /*update the clock*/
+    second_has_passed = false;
+    SensorData.seconds++;
+    SensorData.hours = (SensorData.seconds / 3600) % 24;
+    SensorData.minutes = (SensorData.seconds / 60) % 60;
+    secs = SensorData.seconds % 60;
+
+    inactive_time ++; /*increase inactive time, can cleared ahead if a button was pressed*/
+
+    LCDcmd(0x80);       //first line, first column
+    sprintf(buf, "%02lu:%02lu:%02lu", SensorData.hours, SensorData.minutes, SensorData.secs);
+    LCDstr(buf);
+    while (LCDbusy());
+
+    if( secs%PMON == 0 ){
+      /*update temperature reading*/
+      SensorData.temp = readTC74();
+      LCDcmd(0xc0);       // second line, first column
+      sprintf(buf, "%02d C", SensorData.temp);
+      LCDstr(buf);
+      while (LCDbusy());
+
+      /*update light reading*/
+      SensorData.light = readLight();
+      LCDcmd(0xc0);       // second line, first column
+      LCDpos(0,13);
+      sprintf(buf, "L %1d", SensorData.light); /*limit to 1 character*/
+      LCDstr(buf);
+      while (LCDbusy());
+
+    }
+
+    if(alarms_enabled == true){
+
+      /*check if alarm should go off for time*/
+      if (SensorData.hours == alarm_hour){
+        if(SensorData.minutes == alarm_minute){
+          if(SensorData.seconds == alarm_second){
+            alarm_flag = TIME_ALARM;
+
+          }
+        }
+      }
+
+      /*check if alarm should go off for temperature*/
+      if(SensorData.temp > temp_alarm){
+        alarm_flag = TEMP_ALARM;
+      }
+      
+      /*check if alarm should go off for light*/
+      if(SensorData.light < light_alarm){
+        alarm_flag = LIGHT_ALARM;
+      }
+
+
+      /*resolve alarm situation*/
+      switch (curr_alarm){
+        case TIME_ALARM : break;
+        case TEMP_ALARM : break;
+        case LIGHT_ALRAM: break;
+        default:
+
+
+      }
+
+
+    }
+  }
+        
+  IO_RA7_Toggle();
+  SLEEP();
+  NOP();
+
+}
+
 void resolve_CONFIG_MODE(){}
 void resolve_RECORD_MODE(){}
 void resolve_OUT_OF_BOUNDS(){
@@ -193,7 +272,6 @@ void resolve_OUT_OF_BOUNDS(){
   /*nice job =)*/
   curr_mode = NORMAL_MODE;
 }
-
 
 
 /**
