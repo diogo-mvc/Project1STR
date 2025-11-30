bf94d771c16053c49db232049f0d9e08dc0e8ce9
Barbichas <oeinsteineracareca@gmail.com>
2025-11-30 14:20:36 +0000

    alternative main file with some organization

---
+++ Projeto1.x/main_Carlos.c
@@ -0,0 +1,201 @@
+/**
+  Generated Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  Description:
+    This header file provides implementations for driver APIs for all modules selected in the GUI.
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.81.6
+        Device            :  PIC16F18875
+        Driver Version    :  2.00
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#include "mcc_generated_files/mcc.h"
+#include "I2C/i2c.h"
+#include "LCD/lcd.h"
+#include "stdio.h"
+
+/*
+                         Main application
+ */
+
+//extern volatile bool second_has_passed;
+
+/*--------------------------------  MACROS -------------------------------------*/
+#define PMON 5   /*5 sec monitoring period*/
+#define TALA 3   /*3 seconds duration of alarm signal (PWM)*/
+#define TINA 10  /*10 sec inactivity time*/
+#define ALAF 0   /*alarm flag – alarms initially disabled*/
+#define ALAH 12  /*alarm clock hours*/
+#define ALAM 0   /*alarm clock minutes*/
+#define ALAS 0   /*alarm clock seconds*/
+#define ALAT 20  /*20ºC threshold for temperature alarm*/
+#define ALAL 2   /*threshold for luminosity level alarm */
+#define CLKH 0   /*clock hours*/
+#define CLKM 0   /*clock minutes*/
+
+#define NORMAL_MODE 0
+#define CONFIG_MODE 1
+#define RECORD_MODE 2
+
+/*------------------GLOBAL VARIABLES(good for interrupt handling) ----------------*/
+extern volatile int curr_mode = NORMAL_MODE;
+
+/*---- Function primitves(in C we must declare stuff before calling them, definition is a the bottom)*/
+void resolve_NORMAL_MODE(void);
+void resolve_CONFIG_MODE(void);
+void resolve_RECORD_MODE(void);
+void resolve_OUT_OF_BOUNDS(void);
+
+
+unsigned char readTC74 (void)
+{
+	unsigned char value;
+do{
+	IdleI2C();
+	StartI2C(); IdleI2C();
+    
+	WriteI2C(0x9a | 0x00); IdleI2C();
+	WriteI2C(0x01); IdleI2C();
+	RestartI2C(); IdleI2C();
+	WriteI2C(0x9a | 0x01); IdleI2C();
+	value = ReadI2C(); IdleI2C();
+	NotAckI2C(); IdleI2C();
+	StopI2C();
+} while (!(value & 0x40));
+
+	IdleI2C();
+	StartI2C(); IdleI2C();
+	WriteI2C(0x9a | 0x00); IdleI2C();
+	WriteI2C(0x00); IdleI2C();
+	RestartI2C(); IdleI2C();
+	WriteI2C(0x9a | 0x01); IdleI2C();
+	value = ReadI2C(); IdleI2C();
+	NotAckI2C(); IdleI2C();
+	StopI2C();
+
+	return value;
+}
+
+void main(void)
+{    
+    unsigned char c;
+    char buf[17];
+
+    // initialize the device
+    SYSTEM_Initialize();
+
+
+    unsigned long seconds = 0;
+    unsigned long hours, minutes, secs;
+    // When using interrupts, you need to set the Global and Peripheral Interrupt Enable bits
+    // Use the following macros to:
+
+    // Enable the Global Interrupts
+    INTERRUPT_GlobalInterruptEnable();
+
+    // Enable the Peripheral Interrupts
+    INTERRUPT_PeripheralInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+
+    // Disable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptDisable();
+
+    OpenI2C();
+    //I2C_SCL = 1;
+    //I2C_SDA = 1;
+    //WPUC3 = 1;
+    //WPUC4 = 1;
+    LCDinit();
+
+    while (1)
+    {
+
+      if     (curr_mode == NORMAL_MODE){resolve_NORMAL_MODE();}
+      else if(curr_mode == CONFIG_MODE){resolve_CONFIG_MODE();}
+      else if(curr_mode == RECORD_MODE){resolve_RECORD_MODE();}
+      else{resolve_OUT_OF_BOUNDS();}
+        
+        if (second_has_passed==true) {
+          second_has_passed = false;
+          seconds++;
+          hours = (seconds / 3600);
+          minutes = (seconds / 60) % 60;
+          secs = seconds % 60;
+
+          LCDcmd(0x80);       //first line, first column
+          sprintf(buf, "Time %02lu:%02lu:%02lu", hours, minutes, secs);
+          LCDstr(buf);
+          while (LCDbusy());
+        }//
+        
+        IO_RA7_Toggle();
+        __delay_ms(1000);
+        // SLEEP();
+        //NOP();
+        
+        
+
+
+        /*c = readTC74();
+        LCDcmd(0x80);       //first line, first column
+        while (LCDbusy());
+        LCDstr("Temp");
+        LCDpos(0,8);
+        while (LCDbusy());
+        LCDstr("STR-RTS");
+        LCDcmd(0xc0);       // second line, first column
+        sprintf(buf, "%02d C", c);
+        while (LCDbusy());
+        LCDstr(buf);
+        __delay_ms(2000); */
+    }
+}
+
+void resolve_NORMAL_MODE(){}
+void resolve_CONFIG_MODE(){}
+void resolve_RECORD_MODE(){}
+void resolve_OUT_OF_BOUNDS(){
+  /*no idea how we got here...*/
+  /*nice job =)*/
+  curr_mode = NORMAL_MODE;
+}
+
+
+
+/**
+ End of File
+*/
